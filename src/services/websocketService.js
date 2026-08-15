832bba60b08bad0af80842f499500fa8d51fcc2b
ADMIN <lekhanh10042007@gmail.com>
2026-08-15 08:13:04 +0700

    https to http for localhost url

--- src/services/websocketService.js
+++ src/services/websocketService.js
@@ -1,8 +1,8 @@
 import { openChatModal } from "../pages/ChatPage/chat.js";
 
 
-// const BASE_URL = "ws://localhost:8080/chat?username=";
-const BASE_URL = "wss://localhost:8080/chat?username=";
+const BASE_URL = "ws://localhost:8080/chat?username=";
+// const BASE_URL = "wss://localhost:8080/chat?username=";
 // const BASE_URL = "wss://hidropoietic-unloyally-eleonor.ngrok-free.dev/chat?username=";
 
 class WebSocketService {
