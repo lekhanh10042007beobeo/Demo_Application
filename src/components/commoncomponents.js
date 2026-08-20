c9ba5fde608da77798a3aac322321bc64f1d83dd
ADMIN <lekhanh10042007@gmail.com>
2026-08-20 08:17:09 +0700

    change base url on 20/8/2026

--- src/components/commoncomponents.js
+++ src/components/commoncomponents.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://entered-jerusalem-eddie-grounds.trycloudflare.com";
+export const BASE_URL = "https://uniprotkb-john-pro-consortium.trycloudflare.com";
 
 export const userInfoMap = new Map();
 
